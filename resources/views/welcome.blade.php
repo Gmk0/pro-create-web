348a4ec4be54b9d151570f62ea23491dda593adb
gmk0 <mkgeorges63@gmail.com>
2025-02-08 20:57:04 +0100

    maintenance add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,11 +10,10 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css" rel="stylesheet">
 
     <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
+
     <style>
         /* ! tailwindcss v3.4.1 | MIT License | https://tailwindcss.com */
         *,
@@ -903,7 +902,7 @@
             }
         }
     </style>
-    @endif
+
 </head>
 
 <body class="font-sans antialiased ">
